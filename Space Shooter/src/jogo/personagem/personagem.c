94957b1cae7403160ddec4d68d37fc41ff081ddb
Júlio Evêncio <julioevencio@acad.ifma.edu.br>
2021-04-29 15:36:44 -0300

    Melhorias no código do personagem

--- Space Shooter/src/jogo/personagem/personagem.c
+++ Space Shooter/src/jogo/personagem/personagem.c
@@ -1,12 +1,18 @@
 #include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "../../space_shooter.h"
+#include "../nave/nave.h"
 #include "personagem.h"
 
-struct Tiro {
-    int x;
-    int y;
-    int largura;
-    int altura;
-    int recarregado;
+#define PERSONAGEM_X 50
+#define PERSONAGEM_Y 50
+#define PERSONAGEM_LARGURA 32 * 3
+#define PERSONAGEM_ALTURA 32 * 3
+#define PERSONAGEM_VELOCIDADE 8
+
+struct Personagem {
+    Nave *nave;
     SDL_Texture *textura;
 };
 
@@ -31,32 +37,17 @@ int personagem_criar(Personagem **personagem, SDL_Texture *textura[]) {
         return -1;
     }
 
-    if ((*personagem)->tiro == NULL) {
-        nave_liberar(&(*personagem)->nave);
-        free(*personagem);
-        return -1;
-    }
-
-    (*personagem)->tiro->x = PERSONAGEM_TIRO_X;
-    (*personagem)->tiro->y = PERSONAGEM_TIRO_Y;
-    (*personagem)->tiro->largura = PERSONAGEM_TIRO_LARGURA;
-    (*personagem)->tiro->altura = PERSONAGEM_TIRO_ALTURA;
-    (*personagem)->tiro->recarregado = PERSONAGEM_TIRO_RECARREGADO;
-    (*personagem)->tiro->textura = textura[TEXTURA_SPRITE_PERSONAGEM_2];
-
     (*personagem)->textura = textura[TEXTURA_SPRITE_PERSONAGEM_1];
-    (*personagem)->vida = PERSONAGEM_VIDA;
 
     return 0;
 }
 
 void personagem_liberar(Personagem **personagem) {
-    free((*personagem)->tiro);
     nave_liberar(&(*personagem)->nave);
     free(*personagem);
 }
 
-void personagem_desenhar(SDL_Renderer *tela, Personagem **personagem) {
+void personagem_desenhar(Personagem **personagem, SDL_Renderer *tela) {
     nave_desenhar(tela, (*personagem)->textura, &(*personagem)->nave);
 }
 
@@ -78,32 +69,4 @@ void personagem_parar_subida(Personagem **personagem) {
 
 void personagem_parar_descida(Personagem **personagem) {
     nave_parar_descida(&(*personagem)->nave);
-}
-
-void personagem_atirar(Personagem **personagem) {
-    (*personagem)->tiro->x = (*personagem)->nave->x;
-    (*personagem)->tiro->y = (*personagem)->nave->y;
-    (*personagem)->tiro->recarregado = PERSONAGEM_TIRO_DESCARREGADO;
-}
-
-int personagem_mostrar_tiro(Personagem **personagem) {
-    return (*personagem)->tiro->recarregado;
-}
-
-void personagem_movimentar_tiro(Personagem **personagem) {
-    (*personagem)->tiro->x += PERSONAGEM_TIRO_VELOCIDADE;
-}
-
-void personagem_desenhar_tiro(SDL_Renderer *tela, Personagem **personagem) {
-    SDL_Rect retangulo_tiro = {
-        (*personagem)->tiro->x,
-        (*personagem)->tiro->y,
-        (*personagem)->tiro->largura,
-        (*personagem)->tiro->altura
-    };
-    SDL_RenderCopy(tela, (*personagem)->tiro->textura, NULL, &retangulo_tiro);
-
-    if ((*personagem)->tiro->x > JANELA_LARGURA) {
-        (*personagem)->tiro->recarregado = PERSONAGEM_TIRO_RECARREGADO;
-    }
 }
--- Space Shooter/src/jogo/personagem/personagem.h
+++ Space Shooter/src/jogo/personagem/personagem.h
@@ -1,45 +1,15 @@
 #ifndef PERSONAGEM_H_INCLUDED
 #define PERSONAGEM_H_INCLUDED
 
-#include "../../space_shooter.h"
-#include "../nave/nave.h"
-
-#define PERSONAGEM_X 50
-#define PERSONAGEM_Y 50
-#define PERSONAGEM_LARGURA 32 * 3
-#define PERSONAGEM_ALTURA 32 * 3
-#define PERSONAGEM_VELOCIDADE 8
-#define PERSONAGEM_VIDA 1
-
-#define PERSONAGEM_TIRO_X 50
-#define PERSONAGEM_TIRO_Y 50
-#define PERSONAGEM_TIRO_LARGURA 32
-#define PERSONAGEM_TIRO_ALTURA 32
-#define PERSONAGEM_TIRO_DESCARREGADO 0
-#define PERSONAGEM_TIRO_RECARREGADO 1
-#define PERSONAGEM_TIRO_VELOCIDADE 5
-
-typedef struct Tiro Tiro;
-
-struct Personagem {
-    int vida;
-    Nave *nave;
-    Tiro *tiro;
-    SDL_Texture *textura;
-};
 typedef struct Personagem Personagem;
 
 int personagem_criar(Personagem **personagem, SDL_Texture *textura[]);
 void personagem_liberar(Personagem **personagem);
-void personagem_desenhar(SDL_Renderer *tela, Personagem **personagem);
+void personagem_desenhar(Personagem **personagem, SDL_Renderer *tela);
 void personagem_movimentar(Personagem **personagem);
 void personagem_subir(Personagem **personagem);
 void personagem_descer(Personagem **personagem);
 void personagem_parar_subida(Personagem **personagem);
 void personagem_parar_descida(Personagem **personagem);
-void personagem_atirar(Personagem **personagem);
-int personagem_mostrar_tiro(Personagem **personagem);
-void personagem_movimentar_tiro(Personagem **personagem);
-void personagem_desenhar_tiro(SDL_Renderer *tela, Personagem **personagem);
 
 #endif
