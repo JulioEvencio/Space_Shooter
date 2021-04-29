3c36eaead958821b4adb2450d2383d05197836d7
Júlio Evêncio <julioevencio@acad.ifma.edu.br>
2021-04-29 15:47:02 -0300

    Melhorias no código do jogo

--- Space Shooter/src/jogo/jogo.c
+++ Space Shooter/src/jogo/jogo.c
@@ -1,82 +1,89 @@
 #include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include "../space_shooter.h"
+#include "personagem/personagem.h"
 #include "jogo.h"
 
-void jogo_verificar_tecla_pressionada(SDL_Event *evento, Jogo **jogo);
-void jogo_verificar_tecla_solta(SDL_Event *evento, Jogo **jogo);
+#define JOGO_FUNDO_X 0
+#define JOGO_FUNDO_Y 0
+#define JOGO_FUNDO_LARGURA JANELA_LARGURA
+#define JOGO_FUNDO_ALTURA JANELA_ALTURA
+
+struct Jogo {
+    Personagem *personagem;
+    SDL_Texture *fundo;
+};
+
+void jogo_verificar_tecla_pressionada(Jogo **jogo, SDL_Event *evento);
+void jogo_verificar_tecla_solta(Jogo **jogo, SDL_Event *evento);
 
 int jogo_inicializar(Jogo **jogo, SDL_Texture *textura[]) {
     *jogo = malloc(sizeof **jogo);
 
     if (*jogo == NULL) return -1;
 
-    if (personagem_criar(&(*jogo)->jogador, textura)) {
+    if (personagem_criar(&(*jogo)->personagem, textura)) {
         free(*jogo);
         return -1;
     }
+
+    (*jogo)->fundo = textura[TEXTURA_BACKGROUND];
 
     return 0;
 }
 
 void jogo_liberar(Jogo **jogo) {
-    personagem_liberar(&(*jogo)->jogador);
+    personagem_liberar(&(*jogo)->personagem);
     free(*jogo);
 }
 
-void jogo_tela(SDL_Renderer *tela, SDL_Texture *textura[], Jogo **jogo) {
+void jogo_tela(Jogo **jogo, SDL_Renderer *tela) {
     SDL_Rect fundo = {JOGO_FUNDO_X, JOGO_FUNDO_Y, JOGO_FUNDO_LARGURA, JOGO_FUNDO_ALTURA};
-    SDL_RenderCopy(tela, textura[TEXTURA_BACKGROUND], NULL, &fundo);
+    SDL_RenderCopy(tela, (*jogo)->fundo, NULL, &fundo);
 
-    personagem_desenhar(tela, &(*jogo)->jogador);
-    personagem_movimentar(&(*jogo)->jogador);
-
-    if (!personagem_mostrar_tiro(&(*jogo)->jogador)) {
-        personagem_desenhar_tiro(tela, &(*jogo)->jogador);
-        personagem_movimentar_tiro(&(*jogo)->jogador);
-    }
-
-    /*  Code */
+    personagem_desenhar(&(*jogo)->personagem, tela);
+    personagem_movimentar(&(*jogo)->personagem);
 }
 
-void jogo_evento(SDL_Event *evento, Jogo **jogo) {
+void jogo_evento(Jogo **jogo, SDL_Event *evento) {
     if ((*evento).type == SDL_KEYDOWN) {
-        jogo_verificar_tecla_pressionada(evento, jogo);
+        jogo_verificar_tecla_pressionada(jogo, evento);
     }
 
     if ((*evento).type == SDL_KEYUP) {
-        jogo_verificar_tecla_solta(evento, jogo);
+        jogo_verificar_tecla_solta(jogo, evento);
     }
 }
 
-void jogo_verificar_tecla_pressionada(SDL_Event *evento, Jogo **jogo) {
+void jogo_verificar_tecla_pressionada(Jogo **jogo, SDL_Event *evento) {
     switch ((*evento).key.keysym.sym) {
         case SDLK_UP:
-            personagem_subir(&(*jogo)->jogador);
+            personagem_subir(&(*jogo)->personagem);
             break;
 
         case SDLK_DOWN:
-            personagem_descer(&(*jogo)->jogador);
+            personagem_descer(&(*jogo)->personagem);
             break;
 
         case SDLK_SPACE:
-            if (personagem_mostrar_tiro(&(*jogo)->jogador)) {
-                personagem_atirar(&(*jogo)->jogador);
-            }
+            /*  Code */
             break;
     }
 }
 
-void jogo_verificar_tecla_solta(SDL_Event *evento, Jogo **jogo) {
+void jogo_verificar_tecla_solta(Jogo **jogo, SDL_Event *evento) {
     switch ((*evento).key.keysym.sym) {
         case SDLK_UP:
-            personagem_parar_subida(&(*jogo)->jogador);
+            personagem_parar_subida(&(*jogo)->personagem);
             break;
 
         case SDLK_DOWN:
-            personagem_parar_descida(&(*jogo)->jogador);
+            personagem_parar_descida(&(*jogo)->personagem);
             break;
 
         case SDLK_SPACE:
-            /* puts("tecla espaco solta"); */
+            /*  Code */
             break;
     }
 }
--- Space Shooter/src/jogo/jogo.h
+++ Space Shooter/src/jogo/jogo.h
@@ -1,22 +1,11 @@
 #ifndef JOGO_H_INCLUDED
 #define JOGO_H_INCLUDED
 
-#include "../space_shooter.h"
-#include "personagem/personagem.h"
-
-#define JOGO_FUNDO_X 0
-#define JOGO_FUNDO_Y 0
-#define JOGO_FUNDO_LARGURA JANELA_LARGURA
-#define JOGO_FUNDO_ALTURA JANELA_ALTURA
-
-struct Jogo {
-    Personagem *jogador;
-};
 typedef struct Jogo Jogo;
 
 int jogo_inicializar(Jogo **jogo, SDL_Texture *textura[]);
 void jogo_liberar(Jogo **jogo);
-void jogo_tela(SDL_Renderer *tela, SDL_Texture *textura[], Jogo **jogo);
-void jogo_evento(SDL_Event *evento, Jogo **jogo);
+void jogo_tela(Jogo **jogo, SDL_Renderer *tela);
+void jogo_evento(Jogo **jogo, SDL_Event *evento);
 
 #endif
